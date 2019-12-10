43e2e3cffb15afdd6cb86f7810a9c4786746ae3c
Murali Pulle - Vendor <Murali.Pulle@samsclub.com>
2019-12-10 17:33:34 -0600

    initial changes

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://murali:murali123@ds251948.mlab.com:51948/grphqlpoc';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
